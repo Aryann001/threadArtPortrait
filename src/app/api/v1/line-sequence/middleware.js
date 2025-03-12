6aaf3aff01fef39705c9ba60b42da9b31e6a3871
Aryan <aryanbaghel040@gmail.com>
2025-03-12 12:58:15 +0530

    4th Commit

--- src/app/api/v1/line-sequence/middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  if (req.method === "POST") {
-    req.headers.set("Content-Length", "100mb"); // Increase limit
-  }
-  return NextResponse.next();
-}
